cf669271374a767d7005c9a30c76bacce9637072
Mario-Code7 <marioroa71@yahoo.com>
2025-09-25 10:39:04 -0500

    initial commit

--- src/main/java/com/pluralsight/stringapplication/VariableApp.java
+++ src/main/java/com/pluralsight/stringapplication/VariableApp.java
@@ -2,6 +2,12 @@ package com.pluralsight.stringapplication;
 
 public class VariableApp {
     public static void main(String[] args) {
+        String color = ("red");
+        System.out.println("The color is:""red");
+        int yearClass = 2025;
+        char middleName = 'a';
+        boolean pets = true;
+        String niceMessage = "Make mistakes and learn from them";
 
     }
 }
