4f4131a50ccf7b778935016715c966f1aa1f47fd
Mario-Code7 <marioroa71@yahoo.com>
2025-09-25 12:30:04 -0500

    initial commit

--- src/main/java/com/pluralsight/stringapplication/Reciept.java
+++ src/main/java/com/pluralsight/stringapplication/Reciept.java
@@ -2,5 +2,12 @@ package com.pluralsight.stringapplication;
 
 public class Reciept {
     public static void main(String[] args) {
+        // step one
+        String playstation = "Controllers";
+        double itemPrice = 49.00;
+        int quantityController = 2;
+
+        System.out.println("You bought" + " " + quantityController + " " + playstation + " " + "for" + " " + itemPrice);
+
     }
 }
